4c13237e1d338de0fb3ccd12d0d28f19cd7e2680
doufucheng <1119332482@qq.com>
2019-03-30 13:41:01 +0800

    change

--- data/data.py
+++ data/data.py
@@ -3,6 +3,7 @@ class Data(object):
 
     """
     所有数据的母类，定义了数据的主路径
+    版本控制 https://www.cnblogs.com/feixuelove1009/p/5955332.html
     不同位置的工程，数据存储目录不一致，考虑创建 .gitignore 文件
     https://www.cnblogs.com/wcwnina/p/9112364.html
     """
